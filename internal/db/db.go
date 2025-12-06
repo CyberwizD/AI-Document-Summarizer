154f41c33d5e2c4cb16c85ef4aa49cd194d43c79
WI$DOM <141811303+CyberwizD@users.noreply.github.com>
2025-12-06 04:03:20 +0100

    database: add db package to handle database operations

---
+++ internal/db/db.go
@@ -0,0 +1,20 @@
+package db
+
+import (
+	"log"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+// Open opens a GORM database using the provided DSN.
+func Open(dsn string) *gorm.DB {
+	db, err := gorm.Open(sqlite.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+	if err != nil {
+		log.Fatalf("failed to connect database: %v", err)
+	}
+	return db
+}
